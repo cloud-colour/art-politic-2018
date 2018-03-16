87ade81300c18827da5e1a365cc3ac5197285cfa
Charlie Sittisakundeach <charlie@cloudcolourgames.com>
2018-03-17 02:46:12 +0700

    Add cop spawner

---
+++ Assets/Scripts/Corrupter/Cop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cop : BaseCorrupter {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    override protected void Update () {
+        base.Update();
+	}
+}
