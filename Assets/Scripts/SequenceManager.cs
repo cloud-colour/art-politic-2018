ca6415b41603d7c821da4f1901cd6b9cd637fee1
Mai <mai@pocketplaylab.com>
2018-03-17 03:08:54 +0700

    adjust camera

---
+++ Assets/Scripts/SequenceManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceManager : MonoBehaviour
+{
+	[SerializeField] Animator mainAnim;
+
+	[Header("Debug")]
+	[SerializeField] bool skipIntro;
+	// Use this for initialization
+	void Start ()
+	{
+		if(skipIntro)
+		{
+			mainAnim.SetTrigger("DebugSkipIntro");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
