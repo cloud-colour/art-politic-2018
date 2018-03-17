eb1eb80f34390b21b56ca6b8469a4115a4700a6f
Mai <mai@pocketplaylab.com>
2018-03-17 14:09:29 +0700

    add cop getmoney animation

--- Assets/Scripts/Corrupter/Cop.cs
+++ Assets/Scripts/Corrupter/Cop.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class Cop : BaseCorrupter {
 
+	Animation anim;
+	bool isHappy;
+
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animation>();
+		Invoke("DoRandomStuff",Random.Range(1f,5f));
+	}
+
+	void DoRandomStuff()
+	{
+		if(isHappy)
+			return;
+
+		int rand = Random.Range(1,3);
+		if(rand == 0)//flip facing
+		{
+			transform.localScale = new Vector3(transform.localScale.x*-1,transform.localScale.y,transform.localScale.z);
+		}
+		else //play wiggle 1 or 2
+		{
+			anim.Play("Wiggle"+rand);
+		}
+
+		Invoke("DoRandomStuff",Random.Range(1f,5f));
 	}
 	
 	// Update is called once per frame
@@ -20,9 +42,20 @@ public class Cop : BaseCorrupter {
         {
             if (CorrupterManager.GetCorrupterByID(1).Count == 0)
             {
-                CorrupterManager.Despawn(this);
-                col.gameObject.SetActive(false);
+				isHappy = true;
+				col.gameObject.SetActive(false);
+				this.GetComponent<Collider>().enabled = false;
+
+				transform.localScale = Vector3.one;
+				anim.Stop();
+				anim.Play("Action");
             }
         }
     }
+
+	//called in "Action" animation
+	public void DespawnCorrupter()
+	{
+		CorrupterManager.Despawn(this);
+	}
 }
