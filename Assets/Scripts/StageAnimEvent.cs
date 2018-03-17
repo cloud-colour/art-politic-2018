6c4596bd4e25e10340806aa865ce4b2fe005fa0e
Mai <mai@pocketplaylab.com>
2018-03-17 15:36:16 +0700

    add cash raining in start sequence

--- Assets/Scripts/StageAnimEvent.cs
+++ Assets/Scripts/StageAnimEvent.cs
@@ -11,4 +11,26 @@ public class StageAnimEvent : MonoBehaviour
 		uiAnim.gameObject.SetActive(true);
 		uiAnim.Play();
 	}
+
+	public void SpawnStartMoney()
+	{
+		StartCoroutine(SpawnStartMoneyCoroutine());
+	}
+
+	IEnumerator SpawnStartMoneyCoroutine()
+	{
+		Vector3 tmpPos;
+		for(int i = 0 ;i <= 100; i++)
+		{
+			tmpPos = new Vector3( Random.Range(0,Screen.width/8f),Screen.height , 0);
+			tmpPos = Camera.main.ScreenToWorldPoint(tmpPos);
+			tmpPos.z = 0.5f;
+			var tmpCash = PoolManager.Inst.CreateCash(tmpPos,isProb:true);
+			var body = tmpCash.GetComponent<Rigidbody>();
+			body.sleepThreshold = .5f;
+			body.AddForce(0,-20,0,ForceMode.VelocityChange);
+
+			yield return 0;
+		}
+	}
 }
