6ca28e7ca3c082d6fcf0a89dd25215bc01470341
Charlie Sittisakundeach <charlie@cloudcolourgames.com>
2018-03-17 11:58:53 +0700

    Add test scenes and switch scene functionality

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager {
+
+    static GameSceneManager Inst;
+
+    private int currectScene = 0;
+
+    public static GameSceneManager GetInstance()
+    {
+        if (Inst == null)
+            Inst = new GameSceneManager();
+
+        return Inst;
+    }
+
+    public void GoToNextStage()
+    {
+        if (currectScene == SceneManager.sceneCountInBuildSettings-1)
+            return;
+        currectScene++;
+        LoadCurrentScene();
+    }
+
+    public void GoToPrevStage()
+    {
+        if (currectScene == 0)
+            return;
+        currectScene--;
+        LoadCurrentScene();
+    }
+
+    private void LoadCurrentScene()
+    {
+
+        SceneManager.LoadScene(currectScene);
+    }
+}
