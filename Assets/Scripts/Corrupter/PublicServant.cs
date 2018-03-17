a00910c221a20128af2fae5d206e9306a332c67d
Mai <mai@pocketplaylab.com>
2018-03-17 19:09:33 +0700

    servant animation

--- Assets/Scripts/Corrupter/PublicServant.cs
+++ Assets/Scripts/Corrupter/PublicServant.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class PublicServant : BaseCorrupter {
 
+	Animation anim;
+	bool isHappy;
+
     public float radius;
     public float power;
 
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animation>();
+		Invoke("DoRandomStuff",Random.Range(1f,5f));
 	}
-	
+
+	void DoRandomStuff()
+	{
+		if(isHappy)
+			return;
+
+		int rand = Random.Range(1,3);
+		if(rand == 0)//flip facing
+		{
+			transform.localScale = new Vector3(transform.localScale.x*-1,transform.localScale.y,transform.localScale.z);
+		}
+		else //play wiggle 1 or 2
+		{
+			anim.Play("Wiggle"+rand);
+		}
+
+		Invoke("DoRandomStuff",Random.Range(1f,5f));
+	}
+
 	// Update is called once per frame
     override protected void Update () {
         base.Update();
@@ -30,7 +52,10 @@ public class PublicServant : BaseCorrupter {
 
     public void Die()
     {
+		isHappy = true;
+		anim.Play("Action");
         CorrupterManager.Despawn(this);
-        this.gameObject.SetActive(false);
+		this.GetComponent<Collider>().enabled = false;
+        //this.gameObject.SetActive(false);
     }
 }
