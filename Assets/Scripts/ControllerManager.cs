e3301d0204a8d77d25e5c934c1382f1f505d126a
Charlie Sittisakundeach <charlie@cloudcolourgames.com>
2018-03-18 00:35:07 +0700

    Change Update to FixedUpdate in function to deal with rigidbody

--- Assets/Scripts/ControllerManager.cs
+++ Assets/Scripts/ControllerManager.cs
@@ -30,7 +30,7 @@ public class ControllerManager : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () 
+    void FixedUpdate () 
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
